535dbe7f6e54c06953c4ffbd5f45159fea7d0f37
Lucas Guerra De Azevedo Sardo <rm95084@fiap.com.br>
2022-08-09 11:45:11 -0300

    Create dobroFunc.py

---
+++ dobroFunc.py
@@ -0,0 +1,18 @@
+def it():
+    n = float(input("Digite um número: "))
+    return n
+
+
+def dobro(n):
+    r = n * 2
+    return r
+
+
+def imp(n):
+    print(f"numero: {n/2} \n dobro: {n}")
+
+
+# Principal
+r1 = it()
+d = dobro(r1)
+imp(d)
