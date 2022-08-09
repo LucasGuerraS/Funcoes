33977781ce6469f10e8e0ba18666f823ca17f7e0
Lucas Guerra De Azevedo Sardo <rm95084@fiap.com.br>
2022-08-09 11:46:45 -0300

    Create docExample.py

---
+++ docExample.py
@@ -0,0 +1,22 @@
+def soma(n1, n2):
+    '''
+    Retorna a soma de 2 numeros
+    -----------------------------
+
+    Parametros
+    ------------
+
+    n1, n2: int ou float 
+    Descrição: numeros a serem somados
+
+    Retorno
+    ------------
+    retorna um valor int ou float 
+    Descrição: retorna a soma de n1 + n2
+    '''
+    s = n1 + n2
+    return s
+
+
+x = soma(1, 2)
+print(x)
