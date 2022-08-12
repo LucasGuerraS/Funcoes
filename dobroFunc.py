c4c87f40e83b6f4f9871e87012313dd1ec2ec759
LucasGuerraS <lucasgasardo@gmail.com>
2022-08-12 11:05:49 -0300

    📝 Add functions documentation

--- dobroFunc.py
+++ dobroFunc.py
@@ -1,17 +1,39 @@
 def it():
+    """
+    Função que pede um número e retorna o número
+
+    @parameters: none
+    @return: número (float)
+    """
     n = float(input("Digite um número: "))
     return n
 
-
-def dobro(n):
+def dobro(n: float):
+    """
+    Função que retorna o dobro de um número
+    
+    @parameters: n (float)
+    @return: dobro (float)
+    """
     r = n * 2
     return r
 
+def imp(n: float):
+    """
+    Função que imprime o dobro de um número
 
-def imp(n):
+    @parameters: n (float)
+    @return: none
+    """
     print(f"numero: {n/2} \n dobro: {n}")
 
 def main():
+    """
+    Função principal de execução do programa
+    """
+    n = it()
+    r = dobro(n)
+    imp(r)
     n = it()
     r = dobro(n)
     imp(r)
