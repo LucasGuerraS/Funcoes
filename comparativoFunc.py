3129af3f684e5b5037f80f1a0edb70c7a1f39ee3
LucasGuerraS <lucasgasardo@gmail.com>
2022-08-12 11:12:16 -0300

    📝Add: functions documentation  🚧 Add: main function

--- comparativoFunc.py
+++ comparativoFunc.py
@@ -1,9 +1,21 @@
 def it():
+    """
+    Função que pede um número e retorna o número
+
+    @parameters: none
+    @return: número (float)
+    """
     n = float(input("Digite um número: "))
     return n
 
 
-def maior(n1, n2):
+def maior(n1: float, n2: float):
+    """
+    Função que retorna o maior número entre dois números
+
+    @parameters: n1 (float), n2 (float)
+    @return: texto do maior numero (str)
+    """
     if n1 > n2:
         r = f"numero: {n1} maior que {n2}"
     elif n1 == n2:
@@ -14,10 +26,24 @@ def maior(n1, n2):
 
 
 def imp(ct):
+    """
+    Função que imprime o parametro passado
+
+    @parameters: ct (any) - qualquer coisa que deseje imprimir
+    @return: none
+    """
     print(ct)
 
-#principal
-n1 = it()
-n2 = it()
-q = maior(n1, n2)
-imp(q)
+
+def main():
+    """
+    Função principal de execução do programa
+    """
+    n1 = it()
+    n2 = it()
+    r = maior(n1, n2)
+    imp(r)
+
+
+# principal
+main()
